4ffb3df2f2f9c9599ebeec8ebb721876320a86c4
Joy <HeptagonalTrapezohedron@gmail.com>
2021-05-07 22:20:48 +0900

    20210507

--- src/AbstractFactoryPattern.java
+++ src/AbstractFactoryPattern.java
@@ -3,47 +3,78 @@
  */
 public class AbstractFactoryPattern {
 	public static void main(String[] args) {
-	    SampleFactory factory = new OneFactory(); 
-		factory.CreateProductA();
-		factory.CreateProductB();
+	    SampleFactory Onefactory = new OneFactory(); 
+	    Onefactory.CreateProductA();
+	    Onefactory.CreateProductB();
+	    
+	    SampleFactory Twofactory = new TwoFactory(); 
+	    Twofactory.CreateProductA();
+	    Twofactory.CreateProductB();
     }
 }
 
 abstract class SampleProduct{}
 
-class ProductC extends SampleProduct{
-	 public ProductC() {
-		 System.out.println("生成物件類型A");
+class ProductA1 extends SampleProduct{
+	 public ProductA1() {
+		 System.out.println("生成物件類型A1");
     }
 }
 
-class ProductD extends SampleProduct{
-	 public ProductD() {
-		 System.out.println("生成物件類型B");
+class ProductB1 extends SampleProduct{
+	 public ProductB1() {
+		 System.out.println("生成物件類型B1");
     }
+}
+
+class ProductA2 extends SampleProduct{
+	 public ProductA2() {
+		 System.out.println("生成物件類型A2");
+   }
+}
+
+class ProductB2 extends SampleProduct{
+	 public ProductB2() {
+		 System.out.println("生成物件類型B2");
+   }
 }
 
 interface SampleFactory {
-	ProductC CreateProductA();
-	ProductD CreateProductB();
+	SampleProduct CreateProductA();
+	SampleProduct CreateProductB();
 }
-
 
 
 class OneFactory implements SampleFactory{
 
 	public OneFactory() {
-		 System.out.println("工廠產生");
+		 System.out.println("One工廠產生");
     }
 
 	@Override
-	public ProductC CreateProductA() {
-		return new ProductC();
+	public SampleProduct CreateProductA() {
+		return new ProductA1();
 	}
 
 	@Override
-	public ProductD CreateProductB() {
-		return new ProductD();
+	public SampleProduct CreateProductB() {
+		return new ProductB1();
 	}
 }
 
+class TwoFactory implements SampleFactory{
+
+	public TwoFactory() {
+		 System.out.println("Two工廠產生");
+    }
+
+	@Override
+	public SampleProduct CreateProductA() {
+		return new ProductA2();
+	}
+
+	@Override
+	public SampleProduct CreateProductB() {
+		return new ProductB2();
+	}
+}
